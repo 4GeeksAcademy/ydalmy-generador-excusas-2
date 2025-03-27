d1261669f3381b0447116c6aff45642e54f0d66a
ydalmy <ydalmi.arvelaez@gmail.com>
2025-03-27 21:15:54 +0000

    aun no terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function obternerNumeroAleatorio(max) {
+  return Math.floor(Math.random() * max);
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let arreglo =["my children"  ,   "my brother" , " my Car" , "my friend"];
+  let action= ["ate" , "crash" , "broke" , "paid"]
+  
+  let indice = obternerNumeroAleatorio(arreglo.length);
+  let ind = obternerNumeroAleatorio (action.length);
+  
+  let palabra = arreglo[indice];
+  let word = action[ind];
+  
+  let elementoP = document.getElementById("excuse");
+  elementoP.innerHTML = palabra  + " "  + word ; 
+
+  console.log(palabra);
+
+  
+  
 };
+
